435c8cdc541d80043ad87f5e8bf74bb58da34411
Oleksandr Afanasiev <o.afanasiev@mobidev.biz>
2021-05-06 17:39:08 +0300

    add Expenses component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
@@ -37,9 +37,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      {expenses.map(({ id, title, amount, date }) => (
-        <ExpenseItem key={id} title={title} amount={amount} date={date} />
-      ))}
+      <Expenses expenses={expenses} />
     </div>
   );
 }
