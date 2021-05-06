da9106d0189fd82ec78be448e05a41133f4b5a2f
Oleksandr Afanasiev <o.afanasiev@mobidev.biz>
2021-05-06 16:28:37 +0300

    use props for components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Car Insurance 1',
+      amount: 31.83,
+      date: new Date(2021, 2, 21),
+    },
+    {
+      id: 'e2',
+      title: 'Car Insurance 2',
+      amount: 32.83,
+      date: new Date(2021, 2, 22),
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance 3',
+      amount: 33.83,
+      date: new Date(2021, 2, 23),
+    },
+    {
+      id: 'e4',
+      title: 'Car Insurance 4',
+      amount: 34.83,
+      date: new Date(2021, 2, 24),
+    },
+    {
+      id: 'e5',
+      title: 'Car Insurance 5',
+      amount: 35.83,
+      date: new Date(2021, 2, 25),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem />
+      {expenses.map(({ id, title, amount, date }) => (
+        <ExpenseItem key={id} title={title} amount={amount} date={date} />
+      ))}
     </div>
   );
 }
