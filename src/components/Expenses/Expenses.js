f3781f9dda6f60f6f31205f0ee3762976649a7ba
Oleksandr Afanasiev <o.afanasiev@mobidev.biz>
2021-05-07 07:06:45 +0300

    add year filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 const Expenses = ({ items }) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const selectYearHandler = (year) => {
+    setFilteredYear(year);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={selectYearHandler}
+      />
       <ExpenseItem
         title={items[0].title}
         amount={items[0].amount}
