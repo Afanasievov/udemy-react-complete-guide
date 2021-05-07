7377e54c8c5ffb1329c9197fd289590359fcfb75
Oleksandr <afanasievov@gmail.com>
2021-05-07 11:26:17 +0300

    use dynamic lists

--- src/App.js
+++ src/App.js
@@ -1,41 +1,46 @@
+import { useState } from 'react';
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
-function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Car Insurance 1',
-      amount: 31.83,
-      date: new Date(2021, 1, 21),
-    },
-    {
-      id: 'e2',
-      title: 'Car Insurance 2',
-      amount: 32.83,
-      date: new Date(2021, 2, 22),
-    },
-    {
-      id: 'e3',
-      title: 'Car Insurance 3',
-      amount: 33.83,
-      date: new Date(2021, 3, 23),
-    },
-    {
-      id: 'e4',
-      title: 'Car Insurance 4',
-      amount: 34.83,
-      date: new Date(2021, 4, 24),
-    },
-    {
-      id: 'e5',
-      title: 'Car Insurance 5',
-      amount: 35.83,
-      date: new Date(2021, 5, 25),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Car Insurance',
+    amount: 31.83,
+    date: new Date(2019, 1, 21),
+  },
+  {
+    id: 'e2',
+    title: 'Computer',
+    amount: 132.83,
+    date: new Date(2020, 2, 22),
+  },
+  {
+    id: 'e3',
+    title: 'Chair',
+    amount: 13.83,
+    date: new Date(2020, 3, 23),
+  },
+  {
+    id: 'e4',
+    title: 'Tickets',
+    amount: 4.83,
+    date: new Date(2021, 4, 24),
+  },
+  {
+    id: 'e5',
+    title: 'Toys',
+    amount: 125.09,
+    date: new Date(2021, 5, 25),
+  },
+];
 
-  const addExpenseHandler = (expense) => {};
+function App() {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prev) => [expense, ...prev]);
+  };
 
   return (
     <div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,26 +17,9 @@ const Expenses = ({ items }) => {
         selected={filteredYear}
         onChangeFilter={selectYearHandler}
       />
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
-      />
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      />
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      />
+      {items.map(({ id, title, amount, date }) => (
+        <ExpenseItem key={id} title={title} amount={amount} date={date} />
+      ))}
     </Card>
   );
 };
